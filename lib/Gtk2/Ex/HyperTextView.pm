42eee3af199f7ba8ce25c81eef6e130c10016f12
Chris Weyl <cweyl@alumni.drew.edu>
2012-01-05 12:35:10 -0800

    initial commit

---
+++ lib/Gtk2/Ex/HyperTextView.pm
@@ -0,0 +1,38 @@
+package Gtk2::Ex::HyperTextView;
+
+# ABSTRACT: The great new Gtk2::Ex::HyperTextView!
+
+use Moose;
+use namespace::autoclean;
+use common::sense;
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+L<Smart::Comments>, L<Test::NoSmartComments>
+
+=head1 BUGS
+
+All complex software has bugs lurking in it, and this module is no exception.
+
+Please report any bugs to
+"bug-Gtk2::Ex::HyperTextView@rt.cpan.org",
+or through the web interface at <http://rt.cpan.org>.
+
+Bugs, feature requests and pull requests through GitHub are most welcome; our
+page and repo (same URI):
+
+    https://github.com/RsrchBoy/gtk2::ex::hypertextview
+
+=cut
+
